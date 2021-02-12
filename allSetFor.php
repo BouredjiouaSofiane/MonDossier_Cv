4738f0ef5f0ab43054c3da588a12f3d20e78faeb
Bouredjioua <bouredjiouasofiane@gmail.com>
2021-02-12 04:57:42 +0100

    maj title

--- allSetFor.php
+++ allSetFor.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>A propos de moi</title>
+	<title>GitFlow</title>
 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
